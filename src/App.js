b48639659b71ca5046569adfaedab07d066ba581
cleitonrs <rscleitonrs@gmail.com>
2021-11-18 16:19:20 -0300

    services component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Home from './Home';
 import About from './About';
+import Service from './Service';
 
 function App() {
   return (
     <div className="App">
       <Home />
       <About />
+      <Service />
     </div>
   );
 }
