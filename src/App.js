92a5faca31e169fbe0b22d6dab214b50e32505ce
cleitonrs <rscleitonrs@gmail.com>
2021-11-18 14:56:21 -0300

    about component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Home from './Home';
+import About from './About';
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <About />
     </div>
   );
 }
