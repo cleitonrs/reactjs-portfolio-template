36406a3daf6a64f4a368886cfc96ded6a96ab1b0
cleitonrs <rscleitonrs@gmail.com>
2021-11-22 16:55:03 -0300

    some fixes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className="footer d__flex align__items__center justify__content__space__between pz__10" style={{
       "zIndex": "100", "padding": "10px 20px"
     }}>
-      <img src={footerImg} alt="" className="footer__img pointer" />
+      <img src={footerImg} alt="My pic" className="footer__img pointer" />
       <span className="copyright" style={{"color": "#c6c9d8", "fontSize": "14px", "opacity": "0.75"}}>Copyright © 2021 programmer <a href="https://www.github.com/cleitonrs" target="_blank" rel="noreferrer">Cleiton Santos</a>. All Rights Reserved.</span>
     </div>
   )
